7f287977c8d2657cc930cde31b7643656c4b793c
Denis José <Denis Jos@10.202.236.229>
2018-11-13 20:09:02 -0200

    Comitando para o gitHub

---
+++ Empresa/src/Funcionario.java
@@ -0,0 +1,41 @@
+
+public class Funcionario {
+	
+	private int codFuncionario;
+	private String nome;
+	private String endereco;
+	private String cidade;
+	private String estado;
+	
+	public int getCodFuncionario() {
+		return codFuncionario;
+	}
+	public void setCodFuncionario(int codFuncionario) {
+		this.codFuncionario = codFuncionario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+}
